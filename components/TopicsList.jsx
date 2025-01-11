fa8cd5f182273a9f3a37cc8aa05dac23da9fe9e2
Daniel Medina <paginamedina@gmail.com>
2025-01-10 22:54:12 -0500

    verc tricks

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -2,28 +2,32 @@ import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
 import { HiPencilAlt } from "react-icons/hi";
 
-const getTopics = async () => {
-  try {
-    const res = await fetch("http://localhost:3000/api/topics", {
-      cache: "no-store",
-    });
+// const getTopics = async () => {
+//   try {
+//     const res = await fetch("http://localhost:3000/api/topics", {
+//       cache: "no-store",
+//     });
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch topics");
-    }
+//     if (!res.ok) {
+//       throw new Error("Failed to fetch topics");
+//     }
 
-    return res.json();
-  } catch (error) {
-    console.log("Error loading topics: ", error);
-  }
-};
+//     return res.json();
+//   } catch (error) {
+//     console.log("Error loading topics: ", error);
+//   }
+// };
 
 export default async function TopicsList() {
-  const { topics } = await getTopics();
+  //const { topics } = await getTopics();
 
   return (
     <>
-      {topics.map((t) => (
+    <ul>
+      <li>topx</li>
+      <li>dak</li>
+    </ul>
+      {/* {topics.map((t) => (
         <div
           key={t._id}
           className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
@@ -40,7 +44,7 @@ export default async function TopicsList() {
             </Link>
           </div>
         </div>
-      ))}
+      ))} */}
     </>
   );
 }
