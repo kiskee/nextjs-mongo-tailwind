ddca7b989b9affe04c39bd42eed7a40bd3f9340e
Daniel Medina <paginamedina@gmail.com>
2025-01-10 22:59:04 -0500

    fixing the back

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -4,25 +4,24 @@ import { HiOutlineTrash } from "react-icons/hi";
 import { useRouter } from "next/navigation";
 
 export default function RemoveBtn({ id }) {
-  // const router = useRouter();
-  // const removeTopic = async () => {
-  //   const confirmed = confirm("Are you sure?");
+  const router = useRouter();
+  const removeTopic = async () => {
+    const confirmed = confirm("Are you sure?");
 
-  //   if (confirmed) {
-  //     const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
-  //       method: "DELETE",
-  //     });
+    if (confirmed) {
+      const res = await fetch(`https://nextjs-mongo-tailwind.vercel.app/api/topics?id=${id}`, {
+        method: "DELETE",
+      });
 
-  //     if (res.ok) {
-  //       router.refresh();
-  //     }
-  //   }
-  // };
+      if (res.ok) {
+        router.refresh();
+      }
+    }
+  };
 
   return (
-    // <button onClick={removeTopic} className="text-red-400">
-    //   <HiOutlineTrash size={24} />
-    // </button>
-    <button>remueve</button>
+    <button onClick={removeTopic} className="text-red-400">
+      <HiOutlineTrash size={24} />
+    </button>
   );
 }
--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -2,32 +2,28 @@ import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
 import { HiPencilAlt } from "react-icons/hi";
 
-// const getTopics = async () => {
-//   try {
-//     const res = await fetch("http://localhost:3000/api/topics", {
-//       cache: "no-store",
-//     });
+const getTopics = async () => {
+  try {
+    const res = await fetch("https://nextjs-mongo-tailwind.vercel.app/api/topics", {
+      cache: "no-store",
+    });
 
-//     if (!res.ok) {
-//       throw new Error("Failed to fetch topics");
-//     }
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
 
-//     return res.json();
-//   } catch (error) {
-//     console.log("Error loading topics: ", error);
-//   }
-// };
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+  }
+};
 
 export default async function TopicsList() {
-  //const { topics } = await getTopics();
+  const { topics } = await getTopics();
 
   return (
     <>
-    <ul>
-      <li>topx</li>
-      <li>dak</li>
-    </ul>
-      {/* {topics.map((t) => (
+      {topics.map((t) => (
         <div
           key={t._id}
           className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
@@ -44,7 +40,7 @@ export default async function TopicsList() {
             </Link>
           </div>
         </div>
-      ))} */}
+      ))}
     </>
   );
 }
